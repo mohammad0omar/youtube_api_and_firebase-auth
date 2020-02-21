d0e17f0aca50f6fe3267b2b97f3956c199e341bb
Mohammad Omar <mohammad0omar@outlook.com>
2020-02-21 23:17:10 +0200

    fix: login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
-  constructor(private auth: AuthService, private router: Router) { }
-
-  async doLoginWithGoogle() {
-    await this.auth.loginWithGoogle().then(res =>
-      this.router.navigateByUrl('/login')
-    );
+export class LoginComponent implements OnInit {
+  isLoggedIn: boolean;
+  emitLoggedIn = new Subject<boolean>();
+  constructor(private auth: AuthService, private router: Router) {
+    this.isLoggedIn = false;
+  }
+  ngOnInit() {
+    this.isLoggedIn = false;
+  }
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
+  doLoginWithGoogle() {
+    let _self = this;
+    this.auth.loginWithGoogle(function (res) {
+      if (res)
+        _self.isLoggedIn = true;
+    });
   }
 }
